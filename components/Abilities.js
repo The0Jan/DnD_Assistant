a82cc49ee8db0241153ca0ea5bf43282e6a89054
Jan Walczak <j.walczak@interia.pl>
2022-06-04 19:48:30 +0200

    working abilities description

--- components/Abilities.js
+++ components/Abilities.js
@@ -1,42 +1,122 @@
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView,TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import  Modal  from 'react-native-modal';
+import { useEffect, useState } from 'react';
 
 export default function Abilities({route, navigation}) {
+  const [isModalVisible, setModalVisible] = useState(false)
+  const [lookedUp, setLookedUp] = useState([]);
 
   const {my_character} = route.params;
 
-  function print(thing)
-  {
-    console.log(thing)
-  }
-  
   function Showcase_skills(class_skills)
   {
-
     return(
       <ScrollView>
       {class_skills.map(element => {
         if(element.level <= my_character.level)
         {
-          print(element.name);
-          print(element.level);
-          return(Showcase(element));
+          return(Stat(element, 'grey'));
         }
       })}
       </ScrollView>
     )
   }
 
-  function Showcase(ability)
+  function SetUpModal(ability_text)
+  {
+    setModalVisible(true);
+    setLookedUp(ability_text);
+  }
+
+  function Stat(ability, color)
+  {
+    
+    const statistics = StyleSheet.create({
+      StatRow: {
+        flexDirection: 'row',
+        textAlign:'center',
+        justifyContent:'space-between',
+        backgroundColor: color,
+        marginBottom:10,
+        marginHorizontal:10,
+        borderRadius: 10,
+        borderColor: color,
+        borderWidth: 5,
+      },
+    })
+    return (
+      <TouchableOpacity onPress={()=>SetUpModal(ability.entries) }>
+        <View style={statistics.StatRow}>
+          <View style={styles.AbilityBox}>
+            <Text style={{fontSize:22, color:'white'}}>{ability.level}</Text>
+          </View>
+          <View style={styles.AbilityRest}>
+            <Text style={{fontSize:22}}>{ability.name}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  function ShowModal()
   {
     return(
-      <View style = {styles.drop_list_item}>
-        <View>
-          <Text>Lv.{ability.level}</Text>
-        </View>
-        <View>
-          <Text>{ability.name}</Text>
-        </View>        
+      <View>
+        <Modal isVisible={isModalVisible}>
+        <View style={{alignContent:'center'}}>
+          <ScrollView>
+            <View style={{ 
+                        backgroundColor:'white', 
+                        margin:20, 
+                        borderWidth:10, 
+                        borderColor:'white',
+                        borderRadius:10 }}>
+              {lookedUp.map(text => {
+                if(typeof(text) == 'string')
+                  return(HandleText(text))
+                else if(text.type == 'list')
+                  return(HandleManyTexts(text.items))
+              })}
+            </View>
+          </ScrollView>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <View style={{
+                          backgroundColor:'lightblue',
+                          borderRadius:20,
+                          marginLeft:40,
+                          marginRight:40
+                          }}>
+              <Text style={{textAlign:'center', fontSize:24}}>
+                CLOSE
+              </Text>
+            </View>
+          </TouchableOpacity>
+          </View>
+        </Modal>
+    </View>
+    )
+  }
+
+  function HandleText(text)
+  {
+    return(
+      <View>
+        <Text style={{fontSize:22}}>
+          {text}
+        </Text>
+      </View>
+    )
+  }
+
+  function HandleManyTexts(manytexts)
+  {
+    return(
+      <View>
+      {manytexts.map(element => {
+        return(HandleText(element))
+      })
+      }
       </View>
     )
   }
@@ -45,12 +125,14 @@ export default function Abilities({route, navigation}) {
     <View style={styles.container}>
       <SafeAreaView>
         <View style={styles.item}>
-          <Text>{my_character.class.source}</Text>
-          <Text>{my_character.subclass}</Text>
+          <Text style={{fontSize:28, alignSelf:'center'}}>{my_character.class[0].name}</Text>
         </View>
 
         {Showcase_skills(my_character.classFeatures)}
       </SafeAreaView>
+
+      {ShowModal()}
+
       <View style = {styles.button_create}>
         <Button 
           title="Subclass abilities"
@@ -68,18 +150,18 @@ const text_styles = StyleSheet.create({
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#333',
+      backgroundColor: 'lightgrey',
       alignItems: 'center',
       justifyContent: 'center',
     },
     item: {
       backgroundColor: '#FFF',
-      padding: 10,
-      borderRadius: 10,
-      flexDirection: 'row',
+      padding: 5,
       alignContent: 'center',
-      justifyContent: 'space-between',
-      marginBottom: 20
+      marginBottom: 20,
+      borderColor:'grey',
+      borderWidth:7,
+      borderRadius:10
     },
     button_create: {
       bottom: 15,
@@ -93,13 +175,27 @@ const styles = StyleSheet.create({
     },
     drop_list_item:
     {
-      backgroundColor:'lightgrey',
+      backgroundColor:'white',
       marginBottom:5,
       marginTop:5,
       marginLeft:10,
       marginRight:10,
       flexDirection:'row',
       alignItems:'center',
-      alignContent:'space-between'
+      alignContent:'space-between',
+      borderWidth:30,
+      borderColor:'magenta'
     },
+
+    AbilityBox:{
+      width:"10%",
+      alignItems:'center'
+    },
+    AbilityRest:{
+      borderRadius:5,
+      backgroundColor:'#FFFF',
+      width:"80%",
+      alignItems:'center'
+    }
+
   });
---
+++ components/Popup.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, useState } from 'react-native';
+import { Modal } from 'react-native-modal';
+
+export default function Popup()
+{
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+    };
+  
+    return (
+    <Modal isVisible={isModalVisible}>
+      <View style={{ flex: 1 }}>
+        <Text>Hello!</Text>
+
+        <Button title="Hide modal" onPress={toggleModal} />
+      </View>
+    </Modal>
+    );
+}
