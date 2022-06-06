b9e868c10481e5efa9a3a7f084082f136b0a9263
Jan Walczak <j.walczak@interia.pl>
2022-06-06 23:30:57 +0200

    working get spells

--- components/EditSpells.js
+++ components/EditSpells.js
@@ -1,16 +1,205 @@
-export default function EditSpells({route, navigation}) 
-{
+import { StyleSheet, Text, View, Button, ScrollView,TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import  Modal  from 'react-native-modal';
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function EditSpells({route, navigation}) {
+  const [isModalVisible, setModalVisible] = useState(false)
+  const [lookedUp, setLookedUp] = useState([]);
+  const [avaibleSpells, setSpells] = useState([]);
+  const [spellLevel, setLevel] = useState(0);
+
+  const {meMage} = route.params;
+
+
+  useEffect(() => 
+  {
+    if(avaibleSpells.length == 0)
+    {
+        fetch(`https://5e.tools/data/spells/spells-phb.json`, {method: 'GET'})
+        .then( (response) => response.json())
+        .then( (responseJson) => {setSpells(responseJson.spell)})
+    }
+  });
 
 
 
 
+  function Showcase_spells(my_spells)
+  {
+    return(
+      <ScrollView>
+      {my_spells.map(element => {
+          return(Spell(element, 'grey'));
+      })}
+      </ScrollView>
+    )
+  }
 
+  function SetUpModal(ability_text)
+  {
+    setModalVisible(true);
+    setLookedUp(ability_text);
+  }
 
+  function Spell(ability, color)
+  {
+    
+    const statistics = StyleSheet.create({
+      StatRow: {
+        flexDirection: 'row',
+        textAlign:'center',
+        justifyContent:'space-between',
+        backgroundColor: color,
+        marginBottom:10,
+        marginHorizontal:10,
+        borderRadius: 10,
+        borderColor: color,
+        borderWidth: 5,
+      },
+    })
+    return (
+      <TouchableOpacity onPress={()=>SetUpModal(ability.entries) }>
+        <View style={statistics.StatRow}>
+          <View style={styles.AbilityBox}>
+            <Text style={{fontSize:22, color:'white'}}>{ability.level}</Text>
+          </View>
+          <View style={styles.AbilityRest}>
+            <Text style={{fontSize:22}}>{ability.name}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 
-
-return(
-    <View>
-        
+  function ShowModal()
+  {
+    return(
+      <View>
+        <Modal isVisible={isModalVisible}>
+        <View style={{alignContent:'center'}}>
+          <ScrollView>
+            <View style={{ 
+                        backgroundColor:'white', 
+                        margin:20, 
+                        borderWidth:10, 
+                        borderColor:'white',
+                        borderRadius:10 }}>
+              {lookedUp.map(text => {
+                if(typeof(text) == 'string')
+                  return(HandleText(text))
+                else if(text.type == 'list')
+                  return(HandleManyTexts(text.items))
+              })}
+            </View>
+          </ScrollView>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <View style={{
+                          backgroundColor:'lightblue',
+                          borderRadius:20,
+                          marginLeft:40,
+                          marginRight:40
+                          }}>
+              <Text style={{textAlign:'center', fontSize:24}}>
+                CLOSE
+              </Text>
+            </View>
+          </TouchableOpacity>
+          </View>
+        </Modal>
     </View>
-);
+    )
+  }
+
+  function HandleText(text)
+  {
+    return(
+      <View>
+        <Text style={{fontSize:22}}>
+          {text}
+        </Text>
+      </View>
+    )
+  }
+
+  function HandleManyTexts(manytexts)
+  {
+    return(
+      <View>
+      {manytexts.map(element => {
+        return(HandleText(element))
+      })
+      }
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        {Showcase_spells(avaibleSpells)}
+      </SafeAreaView>
+      {ShowModal()}
+      <View style = {styles.button_create}>
+        </View>
+    </View>
+  );
 }
+
+const text_styles = StyleSheet.create({
+  texts:{}
+})
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'lightgrey',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+      backgroundColor: '#FFF',
+      padding: 5,
+      alignContent: 'center',
+      marginBottom: 20,
+      borderColor:'grey',
+      borderWidth:7,
+      borderRadius:10
+    },
+    button_create: {
+      bottom: 15,
+      paddingHorizontal:15,
+      paddingVertical: 15,
+      justifyContent: 'flex-end'
+    },
+    drop_list:{
+      flexDirection:'column',
+      borderWidth:10,
+    },
+    drop_list_item:
+    {
+      backgroundColor:'white',
+      marginBottom:5,
+      marginTop:5,
+      marginLeft:10,
+      marginRight:10,
+      flexDirection:'row',
+      alignItems:'center',
+      alignContent:'space-between',
+      borderWidth:30,
+      borderColor:'magenta'
+    },
+
+    AbilityBox:{
+      width:"10%",
+      alignItems:'center'
+    },
+    AbilityRest:{
+      borderRadius:5,
+      backgroundColor:'#FFFF',
+      width:"80%",
+      alignItems:'center'
+    }
+
+  });
--- components/Spells.js
+++ components/Spells.js
@@ -1,16 +1,220 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View, Button, ScrollView,TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import  Modal  from 'react-native-modal';
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Spells({route, navigation}) {
+  const [isModalVisible, setModalVisible] = useState(false)
+  const [lookedUp, setLookedUp] = useState([]);
+  const [meMage, setMage] = useState('');
+  const [meSpells, setSpells] = useState([]);
+
+  const {character} = route.params;
 
 
-export default function Spells({route, navigation}) 
-{
+  useEffect(() => {
+    
+    if(meMage == '')
+    {
+        var DoIMage = { name :character.name , class:character.class, spells:'yes'}
+        setMage(JSON.stringify(DoIMage))
+    }
+    else{
+        AsyncStorage.getItem(meMage)
+        .then((value) => {
+            if(value != null){
+                var parsed = JSON.parse(value);
+                setSpells(parsed)}
+            })
+    }
+  });
 
 
 
 
+  function Showcase_spells(my_spells)
+  {
+    return(
+      <ScrollView>
+      {my_spells.map(element => {
+          return(Spell(element, 'grey'));
+      })}
+      </ScrollView>
+    )
+  }
 
-return(
-    <View>
+  function SetUpModal(ability_text)
+  {
+    setModalVisible(true);
+    setLookedUp(ability_text);
+  }
 
+  function Spell(ability, color)
+  {
+    
+    const statistics = StyleSheet.create({
+      StatRow: {
+        flexDirection: 'row',
+        textAlign:'center',
+        justifyContent:'space-between',
+        backgroundColor: color,
+        marginBottom:10,
+        marginHorizontal:10,
+        borderRadius: 10,
+        borderColor: color,
+        borderWidth: 5,
+      },
+    })
+    return (
+      <TouchableOpacity onPress={()=>SetUpModal(ability.entries) }>
+        <View style={statistics.StatRow}>
+          <View style={styles.AbilityBox}>
+            <Text style={{fontSize:22, color:'white'}}>{ability.level}</Text>
+          </View>
+          <View style={styles.AbilityRest}>
+            <Text style={{fontSize:22}}>{ability.name}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  function ShowModal()
+  {
+    return(
+      <View>
+        <Modal isVisible={isModalVisible}>
+        <View style={{alignContent:'center'}}>
+          <ScrollView>
+            <View style={{ 
+                        backgroundColor:'white', 
+                        margin:20, 
+                        borderWidth:10, 
+                        borderColor:'white',
+                        borderRadius:10 }}>
+              {lookedUp.map(text => {
+                if(typeof(text) == 'string')
+                  return(HandleText(text))
+                else if(text.type == 'list')
+                  return(HandleManyTexts(text.items))
+              })}
+            </View>
+          </ScrollView>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <View style={{
+                          backgroundColor:'lightblue',
+                          borderRadius:20,
+                          marginLeft:40,
+                          marginRight:40
+                          }}>
+              <Text style={{textAlign:'center', fontSize:24}}>
+                CLOSE
+              </Text>
+            </View>
+          </TouchableOpacity>
+          </View>
+        </Modal>
     </View>
-);
+    )
+  }
+
+  function HandleText(text)
+  {
+    return(
+      <View>
+        <Text style={{fontSize:22}}>
+          {text}
+        </Text>
+      </View>
+    )
+  }
+
+  function HandleManyTexts(manytexts)
+  {
+    return(
+      <View>
+      {manytexts.map(element => {
+        return(HandleText(element))
+      })
+      }
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+
+
+        {Showcase_spells(meSpells)}
+      </SafeAreaView>
+
+      {ShowModal()}
+
+      <View style = {styles.button_create}>
+        <Button 
+          title="Get New Spells"
+          onPress = { () =>   navigation.navigate('Edit Spells List', {meMage})}
+          />
+        </View>
+    </View>
+  );
 }
+
+const text_styles = StyleSheet.create({
+  texts:{}
+})
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'lightgrey',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+      backgroundColor: '#FFF',
+      padding: 5,
+      alignContent: 'center',
+      marginBottom: 20,
+      borderColor:'grey',
+      borderWidth:7,
+      borderRadius:10
+    },
+    button_create: {
+      bottom: 15,
+      paddingHorizontal:15,
+      paddingVertical: 15,
+      justifyContent: 'flex-end'
+    },
+    drop_list:{
+      flexDirection:'column',
+      borderWidth:10,
+    },
+    drop_list_item:
+    {
+      backgroundColor:'white',
+      marginBottom:5,
+      marginTop:5,
+      marginLeft:10,
+      marginRight:10,
+      flexDirection:'row',
+      alignItems:'center',
+      alignContent:'space-between',
+      borderWidth:30,
+      borderColor:'magenta'
+    },
+
+    AbilityBox:{
+      width:"10%",
+      alignItems:'center'
+    },
+    AbilityRest:{
+      borderRadius:5,
+      backgroundColor:'#FFFF',
+      width:"80%",
+      alignItems:'center'
+    }
+
+  });
